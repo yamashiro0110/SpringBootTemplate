a3bd678b0eaac4ee2e5bf614791a9af1853f72c0
yamashiro.ryota <yamashiro0110@gmail.com>
2016-05-30 22:46:37 +0900

    application.properties追加

---
+++ src/main/java/sample/Application.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yamashiro-r on 2016/05/30.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+DELETE FROM hoge;
+INSERT INTO hoge (id, content) VALUES
+('1', 'hoge'),
+('2', 'fuga'),
+('3', 'piyo');
